6a00d857e2e25670d79a5bd1571255094fe33af7
Andrew Torrez <127915424+BreenDiego23@users.noreply.github.com>
2025-12-31 12:01:38 -0800

    Initial Commit

---
+++ Therapy_Buddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Therapy_Buddy
+//
+//  Created by Andrew Torrez on 12/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Therapy_Buddy/Therapy_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Therapy_BuddyApp.swift
+//  Therapy_Buddy
+//
+//  Created by Andrew Torrez on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Therapy_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
